273efcf2c595f43945285d079d9875fd73646cfa
Brett Baker <GitBaBB@MyBaBB.com>
2023-12-16 17:35:27 -0800

    we fixed the box shadow on the buttons, And i will make this something else and make another gh pages for it. This customer really isnt interested although he said he was,  I  was  only charging 100 bucks for the domain and setup email but he just doesnt get it.  Its too much, he just wants to remain small time I guess.   So I will leave the other gh pages on for a while for him but wont make any improvements.  I have another job now any ways and a Good lead on another.  So we will break it down and turn it into something else to put on our display,  find someone who apreciates it,  I was just doing the guy a favor.  I had high hopes for this guy,  He will remain a labor person

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -9,25 +9,26 @@ const Instagram = () => {
     <div>
       <>
   
-      <div className='OurReelsBoxTruckWrapper relative justify-evenly  flex block      mx-auto mt-[6.75rem] md:mt-[9rem]'>
+      <div className='OurReelsBoxTruckWrapper relative justify-evenly  flex block  mx-auto mt-[6.75rem] md:mt-[9rem]'>
       <a href="https://www.instagram.com/helpinghandservices626">
-         <div className='OurReelsBoxTruck w-[60px] xxs:[70]px xs:w-[80px] relative  content-center border-2 border-[#614829] flex w-fit rounded-[14px]   '>
+         <div className='OurReelsBoxTruck w-[60px] xxs:w-[70]px xs:w-[80px]  relative  content-center border-2 border-[#614829] flex w-fit rounded-[14px]  '>
              <img src={InstagramReels2} alt="Instagram Movers" 
              className='rounded-xl' />
          </div>
-         </a>
+      </a>
      
       <button onClick={() => window.location.href = 'tel:626-371-4331'}>
-            <div className='OldPhoneImage md:hidden w-[60px] xxs:[70]px xs:w-[80px] relative  content-center border-2 border-[#614829] flex w-fit rounded-[14px]   '>
+            <div className='OldPhoneImage md:hidden w-[60px] xxs:w-[70]px xs:w-[80px] relative  content-center border-2 border-[#614829] flex w-fit   rounded-[14px]   '>
                <img src={OldPhoneImg} alt="OldPhoneImg" className='rounded-xl' />
-        </div>
-     </button>
+            </div>
+      </button>
+
 <div className='relative justify-center content-center mx-auto hidden md:block'>
  <p className='text-white font-serif text-3xl mt-2  '><u>626-371-4331</u></p>
  </div>
 
       <a href="https://www.facebook.com/Helpinghandservices626">
-         <div className='OurReelsBoxTruck w-[60px] xxs:[70]px xs:w-[80px] relative  border-2 border-[#614829] flex w-fit rounded-[14px]   '>
+         <div className='OurReelsBoxMover w-[60px] xxs:w-[70]px xs:w-[80px]  relative  border-2 border-[#614829] flex w-fit rounded-[14px]   '>
              <img src={FacebookMover} alt="Facebook Mover" 
              className='rounded-xl' />
          </div>
